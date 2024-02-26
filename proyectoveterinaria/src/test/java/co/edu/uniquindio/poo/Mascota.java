026fe499a6fbbb64644df93e125366eb5922d5f2
DanielValenciaArenas <159465752+DanielValenciaArenas@users.noreply.github.com>
2024-02-25 20:36:36 -0500

    Delete proyectoveterinaria directory

--- proyectoveterinaria/src/test/java/co/edu/uniquindio/poo/Mascota.java
+++
@@ -1,9 +0,0 @@
-package co.edu.uniquindio.poo;
-
-public record Mascota(String nombre, String especie, String raza, double edad, String genero, double peso) {
-
-    public Mascota() {
-        
-    }
-
-}
